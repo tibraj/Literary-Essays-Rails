2b0ce9eebec0ce53bdf3397ad33e0a7acc4ef631
tibraj <tibraj94@gmail.com>
2020-08-15 15:01:20 -0400

    added create action for returning user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,15 @@ class SessionsController < ApplicationController
     def new
     end
 
+    def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            flash[:error] = "Invalid Entry"
+            redirect_to login_path
+        end 
+    end
+
 end
