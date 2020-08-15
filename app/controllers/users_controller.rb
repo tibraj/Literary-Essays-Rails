361c7eda740dda54b9b6673ca9f2d3586d3bb05b
tibraj <tibraj94@gmail.com>
2020-08-15 14:30:39 -0400

    stong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     def new 
     end 
 
+    private 
+
+    def user_params
+        params.require(:user).permit(:email, :name, :password)
+    end 
+    
 end
