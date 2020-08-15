7197dd27e9e41176344d06d27c967783de0db349
tibraj <tibraj94@gmail.com>
2020-08-15 14:34:50 -0400

    helper method for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
         !!session[:user_id]
     end
 
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
 end
