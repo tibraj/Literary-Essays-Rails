92a2b726bebf014b3ec311a74448dbc2b6451eb8
tibraj <tibraj94@gmail.com>
2020-08-17 11:03:08 -0400

    added active rrecord associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord 
+    has_many :essays
+    has_many :responses
+    has_many :essay_responses, through: :responses, source: :essay
     validates :email, presence: true
     has_secure_password
 end
