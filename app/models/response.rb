41d03a590652db00569473bb6d8ebc5b4bac99c8
tibraj <tibraj94@gmail.com>
2020-08-17 11:09:35 -0400

    added activerecord validations

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ApplicationRecord 
     belongs_to :user 
     belongs_to :essay
+    validates :content, presence: true 
+    validates :essay, uniqueness: { scope: :user, message: "You have already responded to this essay." }
 end
