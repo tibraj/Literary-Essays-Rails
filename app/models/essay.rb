6ab8c21c3007f85315a9c18cadd60f29d1a89337
tibraj <tibraj94@gmail.com>
2020-08-17 11:05:29 -0400

    added active rrecord validations

--- app/models/essay.rb
+++ app/models/essay.rb
@@ -4,4 +4,6 @@ class Essay < ApplicationRecord
     has_many :responses
     has_many :responders, through: :responses, source: :user 
     accepts_nested_attributes_for :book 
+    validates :title, presence: true 
+    validates :content, presence: true 
 end
