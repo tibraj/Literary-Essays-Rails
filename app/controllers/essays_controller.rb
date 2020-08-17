8a61fa57be38df9fd11cc3fb42ad480926e6485b
tibraj <tibraj94@gmail.com>
2020-08-17 11:35:59 -0400

    private method for strong parameters

--- app/controllers/essays_controller.rb
+++ app/controllers/essays_controller.rb
@@ -1,6 +1,14 @@
 class EssaysController < ApplicationController 
 
     def new 
+        @essay = Essay.new 
+        @essay.build_book 
     end 
+
+    private 
+
+    def essay_params 
+        params.require(:essay).permit(:title, :content, :book_id, book_attributes: [:title, :author])
+    end
 
 end
