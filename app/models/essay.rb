77aa3c03cc589510ac80160bd4c024edd1a1ee67
tibraj <tibraj94@gmail.com>
2020-08-17 11:04:46 -0400

    added active  record associations

--- app/models/essay.rb
+++ app/models/essay.rb
@@ -1,2 +1,7 @@
 class Essay < ApplicationRecord
+    belongs_to :user
+    belongs_to :book
+    has_many :responses
+    has_many :responders, through: :responses, source: :user 
+    accepts_nested_attributes_for :book 
 end
