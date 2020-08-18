40d665ea6de31f0cc233188d4822eb176c342fa6
tibraj <tibraj94@gmail.com>
2020-08-18 11:16:21 -0400

    private method for setting essay

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,4 +3,12 @@ class ResponsesController < ApplicationController
     def new 
     end
 
+    private
+
+    def set_essay 
+        @essay = Essay.find_by_id(params[:essay_id])
+    end
+
+    
+
 end
