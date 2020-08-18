126f9990d5306cfeccf791a68352a4abcc8a7b13
tibraj <tibraj94@gmail.com>
2020-08-18 11:17:14 -0400

    private method for strong params

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -9,6 +9,8 @@ class ResponsesController < ApplicationController
         @essay = Essay.find_by_id(params[:essay_id])
     end
 
-    
+    def response_params
+        params.require(:repsonse).permit(:content, :essay_id)
+    end  
 
 end
