005adf5b6c91ff66e2f7c0dcde50f8056746c9ce
tibraj <tibraj94@gmail.com>
2020-08-20 11:50:53 -0400

    create user through omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,12 @@ class SessionsController < ApplicationController
         end 
     end
 
+    def omniauth 
+        @user = User.create_by_omniauth(auth)
+        session[:user_id] = @user.id 
+        redirect_to user_path(@user)
+    end
+
     def destroy
         session.delete(:user_id)
         redirect_to '/'
