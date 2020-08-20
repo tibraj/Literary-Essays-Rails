be7a2a3824032d4c7fd41d20a10b72df2d7f8c15
tibraj <tibraj94@gmail.com>
2020-08-20 11:52:38 -0400

    conditional logic for creating user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,17 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(email: params[:user][:email])
-        if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.id 
-            redirect_to user_path(@user)
-        else 
-            flash[:error] = "Invalid Entry"
-            redirect_to login_path
+        if params[:provider]
+            omniauth 
+        else
+            @user = User.find_by(email: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id 
+                redirect_to user_path(@user)
+            else 
+                flash[:error] = "Invalid Entry"
+                redirect_to login_path
+            end
         end 
     end
 
