ec78b3734e2184f48c89305f2c4504802a929b87
tibraj <tibraj94@gmail.com>
2020-08-20 11:57:26 -0400

    fixed syntax

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.user OmniAuth::Builder do 
+Rails.application.config.middleware.use OmniAuth::Builder do 
     provider :github, ENV["GITHUB_CLIENT_ID"],ENV["GITHUB_CLIENT_SECRET"], skip_jwt: true
 end
